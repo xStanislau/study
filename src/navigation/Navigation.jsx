45acb549e5b9ac3c7cb3dbfbe2ee73fb34ef10bc
Stanislau Hancharou <stanislau.hancharou@effective-soft.com>
2019-07-08 19:02:36 +0300

    init commit

---
+++ src/navigation/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import Login from "../pages/Login/components/Login";
+import Main from "../pages/Main/Main";
+export default () => {
+    return (
+        <Router>
+            <Switch>
+                <Redirect exact from="/" to="/auth" />
+                <Route path="/auth" component={Login} />
+                <Route exact path="/main-page" component={Main} />
+                <Route exact component={()=>(<h1>Page not found</h1>)} />
+            </Switch>
+        </Router>
+    );
+};

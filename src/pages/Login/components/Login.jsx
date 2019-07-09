c0e641b388baf482c5df986454edabda2c64471b
Stanislau Hancharou <stanislau.hancharou@effective-soft.com>
2019-07-09 19:15:39 +0300

    add bg image

--- src/pages/Login/components/Login.jsx
+++ src/pages/Login/components/Login.jsx
@@ -1,7 +1,7 @@
-import React, {Fragment, Component} from 'react';
-import Form from './Form/Form'
-import {Link} from "react-router-dom";
-import './Login.css'
+import React, { Fragment, Component } from "react";
+import Form from "./Form/Form";
+import { Link } from "react-router-dom";
+import "./Login.css";
 import Popup from "../../../components/Popup/Popup";
 import Button from "../../../components/Button/Button";
 
@@ -9,32 +9,43 @@ class Login extends Component {
   state = {
     isOpen: false
   };
-  openPopup = (evt) => {
+  openPopup = evt => {
     evt.preventDefault();
-    this.setState({isOpen: true})
+    this.setState({ isOpen: true });
   };
-  closePopup = (evt) => {
+  closePopup = evt => {
     evt.preventDefault();
-    if (evt.target.className == 'popup-bg' || evt.currentTarget.className == 'close') {
-      this.setState({isOpen: false})
+    if (
+      evt.target.className == "popup-bg" ||
+      evt.currentTarget.className == "close"
+    ) {
+      this.setState({ isOpen: false });
     }
   };
 
   render() {
     return (
-        <Fragment>
-          <h2 className="text-center">Log in</h2>
-          <div className="form-wrapper">
-            <Form/>
-            <div className="d-flex align-items-center">
-              <Button onClick={this.openPopup}>modal</Button>
-              <div>-or-</div>
-              <Button ><Link to='/main-page'>redirect</Link></Button>
+      <Fragment>
+        <div className="page-content ">
+          <div className="img">
+            <div className="right-side">
+              <div className="form-wrapper">
+                <h2 className="text-center">Log in</h2>
+                <Form />
+                <div className="d-flex align-items-center">
+                  <Button onClick={this.openPopup}>modal</Button>
+                  <div>-or-</div>
+                  <Button>
+                    <Link to="/main-page">redirect</Link>
+                  </Button>
+                </div>
+                {this.state.isOpen && <Popup onClick={this.closePopup} />}
+              </div>
             </div>
-            {this.state.isOpen && <Popup onClick={this.closePopup}/>}
           </div>
-        </Fragment>
-    )
+        </div>
+      </Fragment>
+    );
   }
 }
 

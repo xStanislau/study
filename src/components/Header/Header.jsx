e002e15c081faa8bebb82b4dd6c81a36a2b8a50e
Stanislau Hancharou <stanislau.hancharou@effective-soft.com>
2019-07-10 15:51:32 +0300

    make header sticky, add styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Header.css";
+import classNames from "classnames";
 
-const Header = props => {
-  const { className, children } = props;
-  return <div className={className}>{children}</div>;
-};
+class Header extends React.Component {
+  state = {};
+
+  handleScroll = () => {
+    this.setState({ scroll: window.scrollY });
+  };
+
+  componentDidMount() {
+    const el = document.querySelector(".header");
+    this.setState({ top: el.offsetTop, height: el.offsetHeight });
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentDidUpdate() {
+    this.state.scroll > this.state.top
+      ? (document.body.style.paddingTop = `${this.state.height}px`)
+      : (document.body.style.paddingTop = 0);
+  }
+
+  render() {
+    const { className, children } = this.props;
+    const fixed = this.state.scroll > this.state.top ? "fixed" : "";
+    const classes = classNames("header", className, { fixed });
+
+    return <div className={classes}>{children}</div>;
+  }
+}
 
 Header.propTypes = {
   className: PropTypes.string,
-  children: PropTypes.node,
-};
-
-Header.defaultProps ={
-    className: 'header'
+  children: PropTypes.node
 };
 
 export default Header;

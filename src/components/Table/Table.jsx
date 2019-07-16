3dbcef79aec9564004add45caf7b0053975f45f7
Stanislau Hancharou <stanislau.hancharou@effective-soft.com>
2019-07-16 15:57:10 +0300

    add table

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Table = props => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {props.cols.map(col => (
+            <th key={col.name}>{col.header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {props.rows.map(row => (
+          <tr key={row.id}>
+            {props.cols.map(col => (
+              <td key={col.name}>{row[col.name]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;

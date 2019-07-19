842dd8122983ff9728437d25e4626787a52a638c
Stanislau Hancharou <stanislau.hancharou@effective-soft.com>
2019-07-19 16:01:28 +0300

    delete unused tag

--- src/pages/Home/components/News/News.jsx
+++ src/pages/Home/components/News/News.jsx
@@ -10,34 +10,32 @@ const News = ({ className }) => {
         <h1 className={`${className}-title text-center mb-5`}>
           News, Blog and Event
         </h1>
-        <section>
-          {posts.map(post => {
-            const { imgSrc, tag, title, creator, text, date } = post;
-            return (
-              <Article
-                imgSrc={imgSrc}
-                className={`${className}-article`}
-                key={`${tag}${title}`}
-                tagText={tag}
-                title={title}
-                creator={creator}
-                date={date}
-                text={text}
-              />
-            );
-          })}
-          <div className="button-group d-flex justify-content-center align-items-center">
-            <Button className="btn btn-outline-light rounded-0">
-              View All News and Blogs
-            </Button>
-            <Button className="btn btn-outline-light rounded-0">
-              View All Events
-            </Button>
-            <Button className="btn btn-outline-light rounded-0">
-              Create Content
-            </Button>
-          </div>
-        </section>
+        {posts.map(post => {
+          const { imgSrc, tag, title, creator, text, date } = post;
+          return (
+            <Article
+              imgSrc={imgSrc}
+              className={`${className}-article`}
+              key={`${tag}${title}`}
+              tagText={tag}
+              title={title}
+              creator={creator}
+              date={date}
+              text={text}
+            />
+          );
+        })}
+        <div className="button-group d-flex justify-content-center align-items-center">
+          <Button className="btn btn-outline-light rounded-0">
+            View All News and Blogs
+          </Button>
+          <Button className="btn btn-outline-light rounded-0">
+            View All Events
+          </Button>
+          <Button className="btn btn-outline-light rounded-0">
+            Create Content
+          </Button>
+        </div>
       </div>
     </section>
   );

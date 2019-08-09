eae6e018abff6656e0e779fb7f29491ef4338be1
Stanislau Hancharou <stanislau.hancharou@effective-soft.com>
2019-08-09 20:39:40 +0300

    restructure navigation

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navigation from "./navigation/Navigation";
+import createStore from "./redux/store/store";
+import Navigator from "./navigation/Navigator";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<Navigation />, document.getElementById("root"));
+const store = createStore();
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Navigator />
+    </Router>
+  </Provider>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

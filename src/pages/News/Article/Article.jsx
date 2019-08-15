245d9d8787f2c1949e2bd0aa297d8e4ea444c815
Stanislau Hancharou <stanislau.hancharou@effective-soft.com>
2019-08-15 12:21:23 +0300

    remove extra wrapper

--- src/pages/News/Article/Article.jsx
+++ src/pages/News/Article/Article.jsx
@@ -13,24 +13,22 @@ const Article = ({
   let article = posts[match.params.id];
   const { title, imgSrc, creator, date, text } = article;
   return (
-    <div className="content-container">
-      <article className="article content">
-        <div className="article__img-wrapper">
-          <img className="article__img" src={imgSrc} alt="Card cap" />
-        </div>
-        <div className="article__description">
-          <h4 className="article__title">{title}</h4>
-          <small className="article__subscription">
-            by <strong className="article__creator">{creator}</strong> on{" "}
-            <strong className="article__date">{date}</strong>
-          </small>
-          <p className="article__text">{text}</p>
-        </div>
-        <Link to={prevPath} className="btn-link">
-          {"<< Back"}
-        </Link>
-      </article>
-    </div>
+    <article className="article content pt-4">
+      <div className="article__img-wrapper">
+        <img className="article__img" src={imgSrc} alt="Card cap" />
+      </div>
+      <div className="article__description">
+        <h2 className="article__title h3">{title}</h2>
+        <small className="article__subscription">
+          by <strong className="article__creator">{creator}</strong> on{" "}
+          <strong className="article__date">{date}</strong>
+        </small>
+        <p className="article__text">{text}</p>
+      </div>
+      <Link to={prevPath} className="btn-link">
+        {"<< Back"}
+      </Link>
+    </article>
   );
 };
 

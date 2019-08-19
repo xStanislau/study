94b2393e927afb6594912cbc0137888ab392fed0
Stanislau Hancharou <stanislau.hancharou@effective-soft.com>
2019-08-19 13:13:12 +0300

    fix init userName from localStorage

--- src/redux/store/persistantStorage.js
+++ src/redux/store/persistantStorage.js
@@ -1,13 +1,11 @@
 export const loadUser = () => {
   if (sessionStorage.getItem("user") !== null) {
     const user = JSON.parse(sessionStorage.getItem("user"));
-    const { isAuthorized, name: userName } = user;
-
+    const { isAuthorized, userName } = user;
     return { auth: { userInfo: { userName }, isAuthorized } };
   } else if (localStorage.getItem("user") !== null) {
     const user = JSON.parse(localStorage.getItem("user"));
     const { isAuthorized, name: userName } = user;
-
     return { auth: { userInfo: { userName }, isAuthorized } };
   } else {
     return undefined;
